734de7c3a3441a78132d17b86e4aebcfb58c10c8
Ryan Rosenfeld <ryro003@gmail.com>
2020-11-28 21:57:36 -0500

    Initial Commit

---
+++ RelationshipTracker/RelationshipTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  RelationshipTrackerApp.swift
+//  RelationshipTracker
+//
+//  Created by Ryan Rosenfeld on 11/28/20.
+//
+
+import SwiftUI
+
+@main
+struct RelationshipTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
